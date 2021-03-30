639fed08454cd4577e7fb78aff29ffd36ace0a30
pless <iliaplesovskikh@gmail.com>
2021-03-30 21:54:05 +0300

    lesson 2/8 https://www.youtube.com/watch?v=XLly5eLlJ5E

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <p>Hello Vue!</p>
+    <v-main-wrapper />
   </div>
 </template>
 
 <script>
-
+import vMainWrapper from './components/v-main-wrapper'
 
 export default {
   name: 'App',
   components: {
-
+    vMainWrapper
   }
 }
 </script>
