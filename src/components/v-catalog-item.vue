73aff72c63bf1d8caae0733b958847021442eff1
pless <iliaplesovskikh@gmail.com>
2021-04-02 02:45:03 +0300

    lesson 2/8 https://www.youtube.com/watch?v=XLly5eLlJ5E
    Changed devDependencies node-sass & sass-loader version

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -12,6 +12,12 @@
   }
 </script>
 
-<style>
+<style lang="scss">
+  .v-catalog-item {
+    flex-basis: 25%;
+    box-shadow: 0 0 8px 0 #e0e0e0;
+    padding: $padding*2;
+    margin-bottom: $margin*2;
+  }
 
 </style>
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -2,16 +2,19 @@
   <div class="v-catalog">
     <h1>Catalog</h1>
     <v-catalog-item/>
+    <v-main-wrapper/>
   </div>
 </template>
 
 <script>
 import vCatalogItem from './v-catalog-item'
+import vMainWrapper from "@/components/v-main-wrapper";
 
 export default {
-  name: "v-catalog",
+  name: 'v-catalog',
   components: {
     vCatalogItem,
+    vMainWrapper,
   }
 }
 </script>
